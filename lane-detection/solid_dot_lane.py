6fd0170d31f32ed4b8b14214851f6288b4a5458e
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-09-05 12:23:57 +0200

    solid dot hough

--- lane-detection/solid_dot_lane.py
+++ lane-detection/solid_dot_lane.py
@@ -2,6 +2,7 @@ import cv2
 import os
 import re
 import json
+import math
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -55,15 +56,40 @@ for dir in dirs: #遍历文件夹
                 x_upper_right = int(1280*max(0., min(Fun_3(para,y_min)+0.05,1.0)))
                 x_lower_left = int(1280*min(1.,max(Fun_3(para,y_max)-0.05,0.0)))
                 x_lower_right = int(1280*max(0.,min(Fun_3(para,y_max)+0.05,1.0)))
-                print("file:{} id:{} ({:.4f},{:.4f}) to ({:.4f},{:.4f})".format(file,lane_id,x_upper_left,x_lower_left,x_upper_right,x_lower_right))
-                mask=np.zeros(vis.shape,dtype=np.uint8)
+                print("file:{} id:{} ({:.0f},{:.0f},{:.0f}) to ({:.0f},{:.0f},{:.0f})".format(file,lane_id,x_upper_left,x_upper_right,y_upper,x_lower_left,x_lower_right,y_lower))
+                masked_image = cv2.GaussianBlur(vis,(5,5),0,0)
+                masked_image = cv2.cvtColor(masked_image,cv2.COLOR_BGR2GRAY)
+                masked_image = cv2.Canny(masked_image,50,159)
+                mask=np.zeros(masked_image.shape,dtype=np.uint8)
                 roi_corners=np.array([[(x_upper_left,y_upper),(x_upper_right,y_upper),(x_lower_right,y_lower),(x_lower_left,y_lower)]],dtype=np.int32)
-                ignore_mask_color = (255,)*vis.shape[2]
+                ignore_mask_color = 255
                 cv2.fillPoly(mask,roi_corners,ignore_mask_color)
-                masked_image=cv2.bitwise_and(vis,mask)
+                masked_image=cv2.bitwise_and(masked_image,mask)
+                ###### hough transformation
+                rho = 1
+                theta = np.pi/180
+                threhold =15
+                minlength = 20
+                maxlengthgap = 20
+                lines = cv2.HoughLinesP(masked_image,rho,theta,threhold,np.array([]),minlength,maxlengthgap)
+#画线
+                linecolor =[0,255,255]
+                linewidth = 2
+                masked_image = cv2.cvtColor(masked_image,cv2.COLOR_GRAY2BGR)
+                if lines is not None:
+                    degree1 = math.degrees(math.atan2(y_upper-y_lower,x_upper_right-x_lower_left))%180
+                    degree2 = math.degrees(math.atan2(y_upper-y_lower,x_upper_left-x_lower_right))%180
+                    print("degree {},{}".format(degree1,degree2))
+                    for line in lines:
+                        for x1,y1,x2,y2 in line:
+                            degree = math.degrees(math.atan2(y2-y1,x2-x1))%180
+                            if degree > min(degree1,degree2) and degree < max(degree1,degree2):
+                                cv2.line(masked_image,(x1,y1),(x2,y2),linecolor,linewidth)
+                '''
                 for i in range(len(data[lane_id]['y'])):
                     p_ori = (int(data[lane_id]['x'][i]*1280),int(data[lane_id]['y'][i]*720))
                     cv2.circle(masked_image,p_ori,5,(0,255,0),-1)
+                '''
                 img_name = cut_img_dir + file[:-4]+'_'+str(lane_id)+".jpg"
                 cv2.imwrite(img_name,masked_image)
         f.close()
