e0932d0b6080d3558cbee369bd533f170785675e
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-10-14 20:44:51 +0200

    cv alg

--- lane-detection/only_mask_try.py
+++ lane-detection/only_mask_try.py
@@ -6,11 +6,46 @@ import math
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from PIL import Image
 from numpy import polyfit,poly1d
 from scipy.optimize import leastsq
 import numpy.linalg as LqA
 from scipy.misc import derivative
 path = "/home/gym/data/img/" #文件夹目录
+
+l_c = 85
+h_c = 160
+
+def Ave_Color(vis,line):
+    half = -1
+    pix = vis.load()
+    for x1,y1,x2,y2 in line:
+        sum = np.array([0,0,0])
+        for i in range(20):
+            y = int(y1 + i/20*(y2-y1))
+            x = max(4,int(x1 + i/20*(x2-x1)))
+            sum += np.array(pix[x-3,y])
+        sum = (sum/20).astype(int)
+        if sum[0] < l_c and sum[1] < l_c and sum[2] < l_c:
+            half = 0
+        if sum[0] >h_c and sum[1] >h_c and sum[2] >h_c:
+            half = 1
+        if half == -1:
+            return False
+        sum = np.array([0,0,0])
+        for i in range(20):
+            y = int(y1 + i/20*(y2-y1))
+            x = min(1276,int(x1 + i/20*(x2-x1)))
+            sum += np.array(pix[x+3,y])
+        sum = (sum/20).astype(int)
+        if half == 1:
+            if sum[0] < l_c and sum[1] < l_c and sum[2] < l_c:
+                return True
+            return False
+        if half == 0:
+            if sum[0] >h_c and sum[1] >h_c and sum[2] >h_c:
+                return True
+            return False
 
 def mkdir(path):
     folder = os.path.exists(path)
@@ -44,6 +79,7 @@ for dir in dirs: #遍历文件夹
         mask_obj = cv2.imread(mask_img_name)
         mask_obj = cv2.cvtColor(mask_obj,cv2.COLOR_BGR2GRAY)
         vis = cv2.imread(img_file_name)
+        vis_color = Image.open(img_file_name)
 
         masked_image = cv2.GaussianBlur(vis,(5,5),0,0)
         masked_image = cv2.cvtColor(masked_image,cv2.COLOR_BGR2GRAY)
@@ -59,7 +95,7 @@ for dir in dirs: #遍历文件夹
         rho = 1
         theta = np.pi/180
         threhold =15
-        minlength = 40
+        minlength = 20
         maxlengthgap = 20
         lines = cv2.HoughLinesP(masked_image,rho,theta,threhold,np.array([]),minlength,maxlengthgap)
 #画线
@@ -79,6 +115,10 @@ for dir in dirs: #遍历文件夹
                     #print("line type {}".format(type(line)))
                     cv_para_data['length'] = math.sqrt((y2-y1)**2+(x2-x1)**2)
                     cv_para_data['degree'] = math.degrees(math.atan2(y2-y1,x2-x1))%180
+                    if cv_para_data['degree']<10 or cv_para_data['degree'] > 170:
+                        continue
+                    if not Ave_Color(vis_color,line):
+                        continue
                     #print("cv_data_para {}".format(cv_para_data))
                     #print("cv_para_data {}".format(cv_para_data))
                     push_data.append(cv_para_data)
